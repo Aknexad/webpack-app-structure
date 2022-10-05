5383ef467869ce4d8386a3ef3c001eff878524af
Aknexad <aknejad@outlook.com>
2022-10-05 11:52:06 +0330

    start

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const _ = require('./styles/main.css');
+
+import home from './pages/home.js';
+
+home();
---
+++ src/pages/home.js
@@ -0,0 +1,6 @@
+export default function home() {
+  const h1 = document.createElement('h1');
+
+  h1.innerText = 'Restouran Page';
+  document.querySelector('#content').appendChild(h1);
+}
